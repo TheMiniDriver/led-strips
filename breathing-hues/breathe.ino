15943713979820c7d758d15f599dd0203bb830e5
Bradley Van Aardt <bradley@indri.co.za>
2021-01-09 18:21:26 +0200

    added breathing sequence

---
+++ breathing-hues/breathe.ino
@@ -0,0 +1,75 @@
+#include <FastLED.h>
+FASTLED_USING_NAMESPACE
+
+#define LED_TYPE WS2812
+#define COLOR_ORDER GRB
+#define NUM_LEDS 59
+#define DATA_PIN 5
+
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  delay(3000); // 3 second delay for recovery
+  FastLED.addLeds<LED_TYPE, DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip); 
+  FastLED.setBrightness(96);
+}
+
+
+int current_led = 0; 
+
+int bright_direction = 1; 
+uint8_t hue = 0;
+uint8_t hue_delta = 12; 
+uint8_t saturation = 128;
+uint8_t saturation_delta = 1;
+uint8_t brightness = 5;
+uint8_t start_hue_val = 0; 
+uint8_t end_hue_val = 255; 
+
+CHSV start_hue; 
+CHSV end_hue;
+ 
+void loop() {
+
+
+      FastLED.show(); 
+      //hue = random8(); 
+
+      EVERY_N_MILLISECONDS(100){
+        if (brightness <80) {
+          start_hue_val++;
+          end_hue_val--;  
+        }
+        addGlitter(5);
+      }
+
+      
+      EVERY_N_MILLISECONDS( 10 ){
+
+         // fill_rainbow (leds, NUM_LEDS, hue, 5);
+          start_hue = CHSV( start_hue_val, saturation, brightness); 
+          end_hue = CHSV(start_hue_val + 50, saturation, brightness);
+          fill_gradient (leds, NUM_LEDS, start_hue, end_hue,SHORTEST_HUES);
+        
+      }
+      
+ 
+      EVERY_N_MILLISECONDS( 40 ) {
+
+        saturation = saturation + saturation_delta; 
+        if (saturation > 250) saturation_delta = -1; 
+        if (saturation <180) saturation_delta = 1; 
+        
+        brightness = brightness + bright_direction; 
+        if (brightness > 180) bright_direction = -1; 
+        if (brightness <50) bright_direction = 1; 
+      }
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::DarkViolet;
+  }
+}
